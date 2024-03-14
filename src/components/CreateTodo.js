797da2d490fc68bae85d59ef25d08536b0c87e46
clairelist <claire.list2501@gmail.com>
2024-03-13 18:27:46 -0600

    anon funcs fun

--- src/components/CreateTodo.js
+++
@@ -1,4 +0,0 @@
-//you can CREATE a new todo here
-//GENERATE or CANCEL clickables
-//similar logic to editing a singular todo,
-//can create a title, a description
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -11,55 +11,51 @@
 //STRETCH goal:: create 'prerequisites' which only allow changing status to DONE when other sub-todos are marked DONE
 
 import './todos.css';
-import Todo from './Todo';
+//import Todo from './Todo';
 import {useState} from 'react';
 
-//dummy data
-const dummyTodos = [
-    {
-     id: 1,
-     title: "Loop over this array of objects",
-     status: "IN PROGRESS",
-     checked: "X"
-    },
-    {
-        id: 2,
-        title: "Do some styling to it",
-        status: "IN PROGRESS",
-        checked: "X"
-       },
-       {
-        id: 3,
-        title: "make status clickable and changes checkmark",
-        status: "IN PROGRESS",
-        checked: "X"
-       }
-]
+//REMEMBER TO KISS:: KEEP IT SIMPLE, STUPID.
+
 
 function Todos(){
+    //REMEMBER:: YOUR CLICK HANDLERS NEED TO BE CALLED AS AN ANON. FUNCTION OR IT WON'T WORK NO I PROMISE YOU IT WON'T.
 
-    const [editing, setEditing] = useState(false);
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
 
-    function handleClick(e){
+    function handleSubmit(e){
         e.preventDefault();
-        // console.log(e.target);
-        setEditing(true);
+        const newTodo = {
+            id: new Date().getTime(),
+            text: todo,
+            status: "IN PROGRESS"
+        }
+        // console.log(e.target.value);
+        setTodos([...todos].concat(newTodo));
+        setTodo("");
     }
     
+    function handleDelete(id){
+        const updatedTodos = [...todos].filter((todo)=>todo.id !== id);
+        setTodos(updatedTodos);
+    }
     
     return (
         <div>
             <h2>TODOS Master View</h2>
             <div className="todos-wrapper">
-             { dummyTodos.map((todo)=>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" onChange={(e)=>setTodo(e.target.value)} value={todo}/>
+                    <button type="submit" >Add TODO</button>
+                </form>
+             { todos.map((todo)=>
                 { 
                 return ( 
-        <div className="todo"> {todo.title} | {todo.status} | {todo.checked}
+        <div className="todo" key={todo.id}> {todo.text} | {todo.status} | <div><button onClick={()=>handleDelete(todo.id)}>Delete TODO.</button></div></div>
         
-        <div className='single-view'>{editing ? <Todo id={todo.id} title={todo.title}  /> : <></>}</div>
-
-        </div>
-    )})}</div>
+    )})}
+    
+    </div>
         </div>
     )
 }
